ca1822bfbe809ac506fbb10926dc845f08bf9b84
Lattecast <rattack.hyde@gmail.com>
2025-01-21 15:51:52 +0100

    Doc: Updated author

--- src/main/java/com/develatter/Main.java
+++ src/main/java/com/develatter/Main.java
@@ -1,6 +1,13 @@
 package com.develatter;
 import com.develatter.infraestructure.server.ChatServer;
 
+/**
+ * An application that simulates a chat server with multiple concurrent clients,
+ * designed for educational purposes to practice architectures, design patterns,
+ * and the implementation of sockets and threads.
+ *
+ * @author Alejandro López Martínez
+ */
 public class Main {
     public static void main(String[] args) {
         new ChatServer();
